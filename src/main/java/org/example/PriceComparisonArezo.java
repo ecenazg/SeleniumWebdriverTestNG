54b67142dce5e1971f188cafe1818bf1d0bb69a2
Ecenaz Güngör <gungor.ecenaz1@gmail.com>
2024-05-16 21:43:25 +0300

    New product analyze added

--- src/main/java/org/example/PriceComparisonArezo.java
+++ src/main/java/org/example/PriceComparisonArezo.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class PriceComparison {
+public class PriceComparisonArezo {
     WebDriver driver;
 
     @BeforeTest
@@ -33,15 +33,15 @@ public class PriceComparison {
     @Test
     public void comparePrices() {
         String[] urls = {
-                "https://www.amazon.com.tr/Apple-iPhone-Pro-Max-256/dp/B0CHXNZ6JP/ref=sr_1_5?crid=2UFDGGEZ85B7V&dib=eyJ2IjoiMSJ9.n3BrJmBGew293415cjxh-LsJ-9u5ZYZqYGC8iP_TBeb0QiuOcC6MJ7K1Afh6pYrl8ZEORYVkUYfXAY7wwqV_sgTx7P4prKyFXaERS8IuY3yC40IoFbjY_xyynVD5tDDmgoUJbQB8Qp90x8pfH4YZu_9n9p7CkhmEFWxbbM02JiEKd0M37_QT8tkdvQrQ5lU1dPHJUgVRcLdskrLBAra915doNSONCB7w_ZHUMLwKLle5yt5G0d6ihJJqP4QihljhbM6IC2s97dYcUbmNRYG_w3as8zSa7-0Ug02h8KylnVI.TpK2u-BtwKMbYOvkUNwikb9vRnFqRc_bINZqlJjH7Y4&dib_tag=se&keywords=iphone+15+pro+max+256+gb&qid=1715617798&sprefix=iphone+15+pro+max%2Caps%2C123&sr=8-5",
-                "https://www.akakce.com/cep-telefonu/en-ucuz-iphone-15-pro-max-fiyati,1745758201.html",
-                "https://www.pttavm.com/iphone-15-pro-max-256-gb-naturel-titanyum-ithalatci-garantili-p-736735070?utm_source=akakce.com&utm_medium=fiyat-kiyaslama&utm_campaign=akakce&v=1.69.2"
+                "https://www.hepsiburada.com/la-roche-posay-anthelios-uvmune-400-spf-50-invisible-fluid-50-ml-pm-HB00000IWKZ8",
+                "https://www.amazon.com.tr/Roche-Anthelios-UVmune-Tinted-Renkli/dp/B09SLGZDXH",
+                "https://www.trendyol.com/la-roche-posay/anthelios-uvmune400-invisible-fluid-spf50-tum-cilt-tipleri-icin-yuz-gunes-kremi-50-ml-p-5770567"
         };
 
         String[] priceSelectors = {
-                "#corePrice_feature_div > div > div > span.a-price.aok-align-center > span:nth-child(2) > span.a-price-whole", // First website CSS selector
-                "#pd_v8 > div.bb_w > span.pb_v8 > span", // Second website CSS selector
-                "#main > div.relative > div > div > div.container > div.flex.flex-col > span > div.flex.flex-col.md\\:flex-row > div.px-4.md\\:pr-0.md\\:pl-5.mt-4.md\\:mt-0.flex.flex-col.flex-1 > div.flex.flex-col.xl\\:flex-row.justify-between.flex-1 > div.md\\:pr-6.flex.flex-col.flex-1.xl\\:w-auto.md\\:mb-3.lg\\:mb-0 > div.flex.leading-none.flex-col.mt-2.md\\:mt-6.mb-2.md\\:mb-0 > div.text-eGreen-700.font-semibold.text-3xl" // Third website CSS selector
+                "#offering-price > span:nth-child(1)", // First website CSS selector
+                "#corePrice_feature_div > div > div > span.a-price.aok-align-center > span:nth-child(2) > span.a-price-whole", // Second website CSS selector
+                "#product-detail-app > div > div.flex-container > div > div:nth-child(2) > div:nth-child(2) > div > div.product-detail-wrapper > div.pr-in-w > div > div > div.product-price-container > div > div > span" // Third website CSS selector
         };
 
         List<Double> prices = new ArrayList<>();
