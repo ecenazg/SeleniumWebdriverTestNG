41745b7d9384c7b3b5faa5cc33569f0503ee459b
Ecenaz Güngör <gungor.ecenaz1@gmail.com>
2024-05-25 19:20:08 +0300

    Insta login test added

---
+++ src/test/java/testLogin/InstagramLoginTest.java
@@ -0,0 +1,62 @@
+package testLogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class InstagramLoginTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver-win64\\chromedriver.exe");
+
+        // Create a new instance of the Chrome driver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testInstagramLogin() {
+        driver.get("https://www.instagram.com/accounts/login/");
+
+        // Wait for the login page to load
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        WebElement passwordField = driver.findElement(By.name("password"));
+
+        // Enter the username and password
+        usernameField.sendKeys("r_ezo.m");
+        passwordField.sendKeys("USI@TEDU0811");
+
+        // Click the login button
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+
+        // Wait for the homepage to load and verify login
+        WebElement profileIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@aria-label='Profile']")));
+        Assert.assertTrue(profileIcon.isDisplayed(), "Profile icon is not displayed, login might have failed.");
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
